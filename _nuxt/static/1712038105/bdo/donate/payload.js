ee8e3d3430ddc431bbad7cf6fcc24bf184a9cfc8
pankami <minhthu701@gmail.com>
2024-04-02 20:59:30 +0700

    up

--- _nuxt/static/1712038105/bdo/donate/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/bdo/donate", {data:[{},{}],fetch:{},mutations:void 0});
--- _nuxt/static/1712038105/bdo/market-tax/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712038105",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbdo\u002Fmarket-tax",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
